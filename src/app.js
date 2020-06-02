f97d9a86eccbd9f17ee01652a5d0f91462aa16b0
Derick <derick@pop-os.localdomain>
2020-06-02 14:43:05 +0300

    more user fields

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const server = http.createServer(app)
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_KEY);
 const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
+  to: 'mudalit@gmail.com',
+  from: 'mudalit@gmail.com',
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
