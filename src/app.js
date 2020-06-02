e1ee979d314836064d022a3aab6434abd9b0eec0
Derick <derick@pop-os.localdomain>
2020-06-02 14:39:20 +0300

    more user fields

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const msg = {
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 // alert('sending...')
-console.log('sending message');
+console.log('sending message ', process.env.SENDGRID_KEY);
 
 sgMail.send(msg).then(res => console.log(res));
 
